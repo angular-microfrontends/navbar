045b63cc6518a44a82f81d5da1055892f15b4f36
Joel Denning <joeldenning@gmail.com>
2020-04-20 17:29:49 -0600

    Beginnings of the navbar app

---
+++ extra-webpack.config.js
@@ -0,0 +1,10 @@
+const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+
+module.exports = config => {
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(config);
+
+  singleSpaWebpackConfig.externals.push('@angular/core', '@angular/platform-browser', '@angular/router', '@angular/common')
+
+  // Feel free to modify this webpack config however you'd like to
+  return singleSpaWebpackConfig;
+};
