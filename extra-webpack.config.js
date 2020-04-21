c614f84d31d411efb6318f9dc571a98521d296f1
Joel Denning <joeldenning@gmail.com>
2020-04-20 18:04:09 -0600

    Removing rxjs / rxjs-operators from bundle

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,7 +3,7 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 module.exports = config => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config);
 
-  singleSpaWebpackConfig.externals.push('@angular/core', '@angular/platform-browser', '@angular/router', '@angular/common')
+  singleSpaWebpackConfig.externals.push('@angular/core', '@angular/platform-browser', '@angular/router', '@angular/common', 'rxjs', 'rxjs/operators')
 
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
