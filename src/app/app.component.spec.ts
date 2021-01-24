46d66a0c28b8415d502bc4750c1816c67c0b4968
daniloesk <47666332+daniloesk@users.noreply.github.com>
2021-01-24 15:29:01 -0300

    Display title and version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
-import { TestBed } from '@angular/core/testing';
+import { VERSION } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,24 +16,26 @@ describe('AppComponent', () => {
         AppComponent,
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should have "navbar" as title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('navbar');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('navbar app is running!');
+    expect(compiled.querySelector('h3').textContent).toBe('navbar');
+  });
+
+  it('should display Angular version', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('div.note').textContent).toBe(`Angular ${VERSION.full}`);
   });
 });
