622c4777f85d7965571f2d41b30a212457f9c018
daniloesk <47666332+daniloesk@users.noreply.github.com>
2021-01-24 12:51:16 -0300

    single-spa-angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyRouteComponent,
   ],
   imports: [
     BrowserModule,
