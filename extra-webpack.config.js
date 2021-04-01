9d77eb6e2813655c1078626801a0a2fedba765e6
daniloesk <47666332+daniloesk@users.noreply.github.com>
2021-04-01 18:30:29 -0300

    @angular-microfrontends scope

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -6,7 +6,7 @@ module.exports = (config, options) => {
 
   singleSpaWebpackConfig.plugins.push(new SystemJSPublicPathWebpackPlugin({
     // ONLY NEEDED FOR WEBPACK 1-4. Not necessary for webpack@5
-    systemjsModuleName: "@angular-mf/navbar"
+    systemjsModuleName: "@angular-microfrontends/navbar"
   }));
 
   // Feel free to modify this webpack config however you'd like to
