562074acdb5ed836833550d8bc7142bbe326c48d
Joel Denning <joeldenning@gmail.com>
2025-02-21 23:22:06 -0700

    Fix tests, add empty route

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, RouterModule.forRoot([])],
       providers: [provideExperimentalZonelessChangeDetection()]
     }).compileComponents();
   });
@@ -16,16 +17,14 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'navbar' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('navbar');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, navbar');
+    const links: (string | null)[] = [];
+    compiled.querySelectorAll('a').forEach(a => {
+      links.push(a.textContent!.trim())
+    });
+    expect(links).toEqual(["Home", "Rate Doggos", "View Doggos"]);
   });
 });
---
+++ src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar-empty-route',
+  imports: [],
+  templateUrl: './empty-route.component.html',
+  styleUrl: './empty-route.component.css'
+})
+export class EmptyRouteComponent {
+
+}
