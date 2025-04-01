14e7ef5d8cd569b8c1e8efdf272ee534123e7c2a
Joel Denning <joeldenning@gmail.com>
2025-03-31 18:35:06 -0600

    Support for single-spa props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { SINGLE_SPA_PROPS, SingleSpaProps } from './single-spa-props';
 
 @Component({
   selector: 'navbar-root',
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: []
 })
 export class AppComponent {
   title = 'angular-mfe-navbar';
+  declare singleSpaProps: SingleSpaProps;
+  constructor(@Inject(SINGLE_SPA_PROPS) singleSpaProps: SingleSpaProps) {
+    this.singleSpaProps = singleSpaProps;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { ApplicationRef } from '@angular/core';
-import { chooseDomElementGetter } from 'dom-element-getter-helpers'
+import { singleSpaAngular } from 'single-spa-angular';
+import { SINGLE_SPA_PROPS } from './app/single-spa-props';
 
-let appRef: ApplicationRef
+export const { bootstrap, mount, unmount } = singleSpaAngular<ExtraProps>({
+  bootstrapApplication,
+  rootComponent: AppComponent,
+  appConfig,
+  propsInjectionToken: SINGLE_SPA_PROPS,
+});
 
-export async function bootstrap() {
-  console.log('bootstrapping');
-}
-
-export async function mount(props: any) {
-  console.log('mounting', props);
-  const domElement = chooseDomElementGetter({}, props)();
-  const rootElement = document.createElement('navbar-root')
-  domElement.appendChild(rootElement);
-  appRef = await bootstrapApplication(AppComponent, appConfig)
-  return appRef;
-}
-
-export async function unmount() {
-  console.log('unmounting');
-  appRef.destroy();
+interface ExtraProps {
 }
